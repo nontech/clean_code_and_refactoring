c1166ea1241958b1f043793fdb8a839508d64b2f
mukesh <mukesh.jaiswal@hypcloud.de>
2022-05-09 15:12:43 +0200

    extract_function: removing 'format' variable

--- statement.js
+++ statement.js
@@ -5,24 +5,27 @@ function statement(invoice, plays) {
   let totalAmount = 0;
   let volumeCredits = 0;
   let result = `Statement for ${invoice.customer}\n`;
-  const format = new Intl.NumberFormat("en-US", {
-    style: "currency",
-    currency: "USD",
-    minimumFractionDigits: 2,
-  }).format;
 
   for (let perf of invoice.performances) {
     volumeCredits += volumeCreditsFor(perf);
 
     // print line for this order
-    result += `  ${playFor(perf).name}: ${format(amountFor(perf) / 100)} (${
+    result += `  ${playFor(perf).name}: ${usd(amountFor(perf) / 100)} (${
       perf.audience
     } seats)\n`;
     totalAmount += amountFor(perf);
   }
-  result += `Amount owed is ${format(totalAmount / 100)}\n`;
+  result += `Amount owed is ${usd(totalAmount / 100)}\n`;
   result += `You earned ${volumeCredits} credits\n`;
   return result;
+}
+
+function usd(aNumber) {
+  return new Intl.NumberFormat("en-US", {
+    style: "currency",
+    currency: "USD",
+    minimumFractionDigits: 2,
+  }).format(aNumber);
 }
 
 function volumeCreditsFor(aPerformance) {
@@ -32,6 +35,7 @@ function volumeCreditsFor(aPerformance) {
     result += Math.floor(aPerformance.audience / 5);
   return result;
 }
+
 function playFor(aPerformance) {
   return plays[aPerformance.playID];
 }
